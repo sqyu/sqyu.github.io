0c8e64cc82f9cd91715980e4fcc99e0c3041ab2a
sqyu <sqyu@uw.edu>
2020-06-27 03:41:10 +0900

    Added navigators in Travel sub-pages

--- Travel/template.py
+++ Travel/template.py
@@ -55,6 +55,10 @@ country_flag = OrderedDict(
 	"al": ("🇦🇱", "Albania", "Shqipëria")
 	})
 
+country_sorted_by_en = sorted(country_flag.items(), key=lambda item: item[1][1])
+country_sorted_by_en = [code for code, _ in country_sorted_by_en]
+country_sorted_by_en_idx_dict = {code: index for index, code in enumerate(country_sorted_by_en)}
+
 
 open("travel_list.jemdoc", "w").write(
 """# jemdoc: menu{MENU}{travel_list.html}, lang-en
@@ -63,14 +67,17 @@ open("travel_list.jemdoc", "w").write(
 == Countries and regions visited
 """ + 
 "\n".join([
-	"- " + items[0] + " [" + country + ".html " + items[1] + " (" + items[2] + ")]"
-	for country, items in country_flag.items()
+	"- " + flag + " [" + country + ".html " + english + " (" + native + ")]"
+	for country, (flag, english, native) in country_flag.items()
 	]) + "\n")
 
 
 country_sub_template = """# jemdoc: menu{MENU}{COUNTRY_CODE.html}, lang-en
 = Travel to {COUNTRY}
+
 [travel_list.html Back to list]
+
+{PREV_NEXT_COUNTRIES}
 
 == Description
 {DESCRIPTION}
@@ -79,15 +86,50 @@ country_sub_template = """# jemdoc: menu{MENU}{COUNTRY_CODE.html}, lang-en
 {PHOTOS}
 """
 
+def prev_next_countries(prev_visit_idx, next_visit_idx, prev_alpha_idx, next_alpha_idx):
+	if prev_visit_idx == -1:
+		prev_visit = None
+	else:
+		prev_visit = "[" + list(country_flag.items())[prev_visit_idx][0] + ".html <-Previous by order of first visit<-]"
+	if prev_alpha_idx == -1:
+		prev_alpha = None
+	else:
+		prev_alpha = "[" + country_sorted_by_en[prev_alpha_idx] + ".html <-Previous by alphabetical order<-]"
+	if next_visit_idx == len(country_flag):
+		next_visit = None
+	else:
+		next_visit = "[" + list(country_flag.items())[next_visit_idx][0] + ".html ->Next by order of first visit->]"
+	if next_alpha_idx == len(country_flag):
+		next_alpha = None
+	else:
+		next_alpha = "[" + country_sorted_by_en[next_alpha_idx] + ".html ->Next by alphabetical order->]"
+	if prev_alpha is None:
+		output = next_alpha + "\n\n"
+	elif next_alpha is None:
+		output = prev_alpha + "\n\n"
+	else:
+		output = prev_alpha + "{{&emsp;&emsp;&emsp;}}" + next_alpha + "\n\n"
+	if prev_visit is None:
+		output += next_visit
+	elif next_visit is None:
+		output += prev_visit
+	else:
+		output += prev_visit + "{{&emsp;&emsp;&emsp;}}" + next_visit
+	return output
 
-for country, items in country_flag.items():
+
+
+
+for index, (country, (flag, english, native)) in enumerate(country_flag.items()):
 	if country + ".txt" not in os.listdir("."):
 		open(country + ".txt", "w").write("DESCRIPTION:\n\nPHOTOS:\n")
 	desc, photos = "".join(open(country + ".txt").readlines()).split("\nPHOTOS:\n")
 	desc = re.sub(r"^DESCRIPTION:\n", "", desc).strip()
 	photos = photos.strip()
 	jemdoc_write = country_sub_template.replace("COUNTRY_CODE", country)
-	jemdoc_write = jemdoc_write.replace("{COUNTRY}", items[0] + " " + items[1] + " (" + items[2] + ")")
+	jemdoc_write = jemdoc_write.replace("{COUNTRY}", flag + " " + english + " (" + native + ")")
+	jemdoc_write = jemdoc_write.replace("{PREV_NEXT_COUNTRIES}", 
+		prev_next_countries(index-1, index+1, country_sorted_by_en_idx_dict[country]-1, country_sorted_by_en_idx_dict[country]+1))
 	if desc:
 		jemdoc_write = jemdoc_write.replace("{DESCRIPTION}", desc)
 	else:
